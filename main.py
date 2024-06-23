781d44a3f5a7181ee44fe2241847230719dc28bd
Danis <dandijar@yandex.ru>
2024-06-23 13:33:16 +0500

    fastAPI second try

--- main.py
+++ main.py
@@ -1,40 +1,59 @@
+from contextlib import asynccontextmanager
 from typing import Optional
 import uvicorn
-from pydantic import BaseModel
-from fastapi import FastAPI
+from fastapi.openapi.models import Response
+from fastapi import FastAPI, Request
+from http import HTTPStatus
+import os
+from telegram import Update
+from telegram.ext import MessageHandler, Application, filters
 
-from bot import update_bot
+TOKEN = os.getenv("TELEGRAM_TOKEN")
+FORBIDDEN_WORD = os.getenv("HAMSTER_URL_BASE")
+WEBHOOK_BASE = os.getenv("WEBHOOK_BASE")
 
-app = FastAPI()
+
+def delete_messages_with_hamster(update, context):
+    print("new message")
+    message_text = update.message.text.lower()
+    if FORBIDDEN_WORD in message_text:
+        context.bot.delete_message(chat_id=update.effective_chat.id, message_id=update.message.message_id)
 
 
-class TelegramWebhook(BaseModel):
-    update_id: int
-    message: Optional[dict]
-    edited_message: Optional[dict]
-    channel_post: Optional[dict]
-    edited_channel_post: Optional[dict]
-    inline_query: Optional[dict]
-    chosen_inline_result: Optional[dict]
-    callback_query: Optional[dict]
-    shipping_query: Optional[dict]
-    pre_checkout_query: Optional[dict]
-    poll: Optional[dict]
-    poll_answer: Optional[dict]
+telegram_bot = (Application.builder()
+                .updater(None)
+                .token(TOKEN)
+                .read_timeout(7)
+                .get_updates_read_timeout(42)
+                .build())
+telegram_bot.add_handler(MessageHandler(filters.TEXT, delete_messages_with_hamster))
+
+
+@asynccontextmanager
+async def lifespan(_: FastAPI):
+    await telegram_bot.bot.setWebhook(f"https://api.telegram.org/bot{TOKEN}/"
+                                      f"setWebhook?url={WEBHOOK_BASE}")
+    async with telegram_bot:
+        await telegram_bot.start()
+        yield
+        await telegram_bot.stop()
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 @app.get("/")
-def index():
+async def index():
     return {"message": "bot works"}
 
 
-@app.post("/webhook")
-def webhook(webhook_data: TelegramWebhook):
-    update_bot(webhook_data)
-    # updater.start_polling()
-    # updater.idle()
-    return {"message": "ok"}
+@app.post("/")
+async def process_update(request: Request):
+    request_json = await request.json()
+    update = Update.de_json(request_json, telegram_bot.bot)
+    await telegram_bot.process_update(update)
+    return Response(status_code=HTTPStatus.OK)
 
 
 if __name__ == '__main__':
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
