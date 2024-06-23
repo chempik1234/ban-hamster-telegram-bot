884fdbfc26c76325a4fabd8e8efbc800cfed8be9
Danis <dandijar@yandex.ru>
2024-06-23 14:16:46 +0500

    words list

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 from contextlib import asynccontextmanager
 from typing import Optional
 import uvicorn
@@ -9,14 +10,23 @@ from telegram import Update
 from telegram.ext import MessageHandler, Application, filters
 
 TOKEN = os.getenv("TELEGRAM_TOKEN")
-FORBIDDEN_WORD = os.getenv("HAMSTER_URL_BASE")
+with open("ban_words.json", 'r') as f:
+    FORBIDDEN_WORDS = json.load(f)  # os.getenv("HAMSTER_URL_BASE")
 WEBHOOK_BASE = os.getenv("WEBHOOK_BASE")
+with open("deletion_message.txt", "r") as f:
+    DELETION_MESSAGE_TEMPLATE = f.readline()
+
+
+def get_hamster_message_text(username):
+    return DELETION_MESSAGE_TEMPLATE.replace("<>", username)
 
 
 async def delete_messages_with_hamster(update, context):
-    print("new message")
     message_text = update.message.text.lower()
-    if FORBIDDEN_WORD in message_text:
+    if any(i.lower() in message_text for i in FORBIDDEN_WORDS):
+        user = update.effective_user
+        username = user.first_name or user.username
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=get_hamster_message_text(username))
         await context.bot.delete_message(chat_id=update.effective_chat.id, message_id=update.message.message_id)
 
 
